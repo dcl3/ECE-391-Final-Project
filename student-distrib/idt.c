6e5d62bd2d73fdeaed88ab29b19679c94c8ba26e
afrer21 <adamdf2@illinois.edu>
2023-03-20 10:52:43 -0500

    intel exceptions added

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -11,6 +11,7 @@ void idt_init(void) {
     int i;
 
     for (i = 0; i < NUM_VEC; i++) {
+        // traps then interrupts
         if (i < 0x20) {
             idt[i].seg_selector = KERNEL_CS;
             idt[i].reserved4 = 0;
@@ -34,12 +35,119 @@ void idt_init(void) {
         }
     }
 
-    SET_IDT_ENTRY(idt[0], de);
+    SET_IDT_ENTRY(idt[DE], de);
+    SET_IDT_ENTRY(idt[DB], db);
+    SET_IDT_ENTRY(idt[NMI], nmi);
+    SET_IDT_ENTRY(idt[BP], bp);
+    SET_IDT_ENTRY(idt[OF], of);
+    SET_IDT_ENTRY(idt[BR], br);
+    SET_IDT_ENTRY(idt[UD], ud);
+    SET_IDT_ENTRY(idt[NM], nm);
+    SET_IDT_ENTRY(idt[DF], df);
+    SET_IDT_ENTRY(idt[CSO], cso);
+    SET_IDT_ENTRY(idt[TS], ts);
+    SET_IDT_ENTRY(idt[NP], np);
+    SET_IDT_ENTRY(idt[SFE], sfe);
+    SET_IDT_ENTRY(idt[GP], gp);
+    SET_IDT_ENTRY(idt[PF], pf);
+    SET_IDT_ENTRY(idt[MF], mf);
+    SET_IDT_ENTRY(idt[AC], ac);
+    SET_IDT_ENTRY(idt[MC], mc);
+    SET_IDT_ENTRY(idt[XF], xf);
 
-    printf("size: %d\n", idt[0].size);
 }
 
 void de(void) {
     printf("Divide Error Exception\n");
+    while(1){};
+}
+
+void db(void) {
+    printf("Debug Exception\n");
+    while(1){};
+}
+
+void nmi(void) {
+    printf("NMI Interrupt\n");
+    while(1){};
+}
+
+void bp(void) {
+    printf("Breakpoint Exception\n");
+    while(1){};
+}
+
+void of(void) {
+    printf("Overflow Exception\n");
+    while(1){};
+}
+
+void br(void) {
+    printf("BOUND Range Exceeded Exception\n");
+    while(1){};
+}
+
+void ud(void) {
+    printf("Invalid Opcode Exception\n");
+    while(1){};
+}
+
+void nm(void) {
+    printf("Device Not Available Exception\n");
+    while(1){};
+}
+
+void df(void) {
+    printf("Double Fault Exception\n");
+    while(1){};
+}
+
+void cso(void) {
+    printf("Coprocessor Segment Overrun\n");
+    while(1){};
+}
+
+void ts(void) {
+    printf("Invalid TSS Exception\n");
+    while(1){};
+}
+
+void np(void) {
+    printf("Segment Not Present\n");
+    while(1){};
+}
+
+void sfe(void) {
+    printf("Stack Fault Exception\n");
+    while(1){};
+}
+
+void gp(void) {
+    printf("General Protection Exception\n");
+    while(1){};
+}
+
+void pf(void) {
+    printf("Page-Fault Exception\n");
+    while(1){};
+}
+
+void mf(void) {
+    printf("x87 FPU Floating-Point Error\n");
+    while(1){};
+}
+
+void ac(void) {
+    printf("Alignment Check Exception\n");
+    while(1){};
+}
+
+void mc(void) {
+    printf("Machine-Check Exception\n");
+    while(1){};
+}
+
+void xf(void) {
+    printf("SIMD Floating-Point Exception\n");
     while(1){};
 }
