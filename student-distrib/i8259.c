fefa1a1113c92bb27ec26814cbb895f22f25a012
afrer21 <adamdf2@illinois.edu>
2023-03-19 21:36:18 -0500

    untested pic code

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -9,18 +9,85 @@
 uint8_t master_mask; /* IRQs 0-7  */
 uint8_t slave_mask;  /* IRQs 8-15 */
 
+// adapted from lecture 10
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    cli();
+
+    // mask all interrupts
+    outb(IRQ_ALL_MASK, MASTER_8259_DATA);
+    outb(IRQ_ALL_MASK, SLAVE_8259_DATA);
+
+    // init control word sequence for master
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW2_MASTER, MASTER_8259_DATA);
+    outb(ICW3_MASTER, MASTER_8259_DATA);
+    outb(ICW4, MASTER_8259_DATA);
+
+    // init control word sequence for slave
+    outb(ICW1, SLAVE_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_DATA);
+    outb(ICW3_SLAVE, SLAVE_8259_DATA);
+    outb(ICW4, SLAVE_8259_DATA);
+
+    // mask all interrupts
+    outb(IRQ_ALL_MASK, MASTER_8259_DATA);
+    outb(IRQ_ALL_MASK, SLAVE_8259_DATA);
+
+    // unmask IR2 on master for slave connection
+    enable_irq(IR2);
+    sti();
+
 }
 
+// adapted from OS Dev, https://wiki.osdev.org/8259_PIC
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+    uint16_t port;
+    uint8_t value;
+
+    // determine which 8259
+    if (irq_num < NUM_IR) {
+        port = MASTER_8259_DATA;
+    } else {
+        port = SLAVE_8259_DATA;
+        irq_num -= NUM_IR;
+    }
+
+    // replace irq mask with 0 (unmasked)
+    value = inb(port) & ~(1 << irq_num);
+    outb(value, port);
 }
 
+// adapted from OS Dev, https://wiki.osdev.org/8259_PIC
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+    uint16_t port;
+    uint8_t value;
+
+    // determine which 8259
+    if (irq_num < NUM_IR) {
+        port = MASTER_8259_DATA;
+    } else {
+        port = SLAVE_8259_DATA;
+        irq_num -= NUM_IR;
+    }
+
+    // replace irq mask with 1 (masked)
+    value = inb(port) | (1 << irq_num);
+    outb(value, port);
 }
 
+// adapted from OS Dev, https://wiki.osdev.org/8259_PIC
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+    // determine if slave 8259
+    if (irq_num >= NUM_IR) {
+        // EOI for both master IR2 and slave irq
+        outb(EOI | (irq_num - NUM_IR), SLAVE_8259_PORT);
+        outb(EOI | IR2, MASTER_8259_PORT);
+    } else {
+        // EOI for just slave
+        outb(EOI | irq_num, MASTER_8259_PORT);
+    }
 }
