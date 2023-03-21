83dda89780171ab04d4802a6bcb7d016f2aa6291
afrer21 <adamdf2@illinois.edu>
2023-03-20 23:57:05 -0500

    not working

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,9 +1,64 @@
 #include "paging.h"
 
-
-
 void paging_init(){
+    //set each entry to not present
+    int i;
+    for(i = 0; i < 1024; i++)
+    {
+        // This sets the following flags to the pages:
+        //   Supervisor: Only kernel-mode can access them
+        //   Write Enabled: It can be both read from and written to
+        //   Not Present: The page table is not present
+        page_directory[i] = 0x00000002;
+    }
 
+    // holds the physical address where we want to start mapping these pages to.
+    // in this case, we want to map these pages to the very beginning of memory.
+    
+    //we will fill all 1024 entries in the table, mapping 4 megabytes
+    for(i = 0; i < 1024; i++)
+    {
+        // As the address is page aligned, it will always leave 12 bits zeroed.
+        // Those bits are used by the attributes ;)
+        first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+    }
 
+    // attributes: supervisor level, read/write, present
+    page_directory[0] = ((unsigned int)first_page_table) | 3;
+
+    loadPageDirectory(page_directory);
+    enablePaging();
+
+}
+
+void loadPageDirectory(unsigned int *pageDirectory) {
+  asm volatile (
+    "push %%ebp \n\t"
+    "mov %%esp, %%ebp \n\t"
+    "mov 8(%%esp), %%eax \n\t"
+    "mov %%eax, %%cr3 \n\t"
+    "mov %%ebp, %%esp \n\t"
+    "pop %%ebp \n\t"
+    "ret \n\t"
+    :
+    : "r" (pageDirectory)
+    : "%eax"
+  );
+}
+
+void enablePaging() {
+  asm volatile (
+    "push %%ebp \n\t"
+    "mov %%esp, %%ebp \n\t"
+    "mov %%cr0, %%eax \n\t"
+    "or $0x80000000, %%eax \n\t"
+    "mov %%eax, %%cr0 \n\t"
+    "mov %%ebp, %%esp \n\t"
+    "pop %%ebp \n\t"
+    "ret \n\t"
+    :
+    :
+    : "%eax"
+  );
 }
 
