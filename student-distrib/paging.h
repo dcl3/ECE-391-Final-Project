ccf848bea8b235cac751d92b557d82b24dcde9fa
afrer21 <adamdf2@illinois.edu>
2023-04-10 14:57:20 -0500

    initial structures and paging attempt

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -10,14 +10,18 @@
 
 #define KERNEL_ADDR 0x00400000
 #define FOUR_KB 0x00001000
+#define FOUR_MB 0x00400000
 
 #define UPPER_TEN 0xffc00000
 #define UPPER_TWENTY 0xfffff00
 
 #define READ_WRITE_P 0x3
 #define READ_WRITE_P_FOUR_MB 0x83
+#define USER_READ_WRITE_P_FOUR_MB 0x87
 
 #define VIDEO_ADDR 0xb8000
+
+#define USER_ADDR 0x00800000
 
 uint32_t page_directory[NUM_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
 
---
+++ student-distrib/task_struct.h
@@ -0,0 +1,32 @@
+/* task_struct.h - Defines used in tasks
+ * vim:ts=4 noexpandtab
+ */
+
+#include "types.h"
+
+#define MAX_FD 8
+
+struct f_op_tbl_ptr {
+    int32_t (*open)(const uint8_t* filename);
+    int32_t (*read) (int32_t fd, void* buf, int32_t nbytes);
+    int32_t (*write) (int32_t fd, const void* buf, int32_t nbytes);
+    int32_t (*close) (int32_t fd);
+} f_op_tbl_ptr_t;
+
+struct f_array {
+    f_op_tbl_ptr_t* f_op_tbl_ptr;
+    uint32_t inode;
+    uint32_t f_pos;
+    uint32_t flags;
+} f_array_t;
+
+struct pcb {
+    uint32_t id;
+    uint32_t parent_id;
+    f_array_t f_array[MAX_FD];
+    uint32_t saved_esp;
+    uint32_t saved_ebp;
+    uint32_t active;
+} pcb_t;
+
+#endif /* _task_struct_H */
